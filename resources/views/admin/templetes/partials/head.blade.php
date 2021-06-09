f41767391080d5f3063c55aa3c179d62af63986e
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-06-09 22:53:52 +0100

    added new admin templete

--- resources/views/admin/templetes/partials/head.blade.php
+++ resources/views/admin/templetes/partials/head.blade.php
@@ -1,15 +1,9 @@
 <head>
 
 
-
-
-
-
-
-
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{url('../assets/img/apple-icon.png')}}">
+    <link rel="icon" type="image/png" href="{{url('../assets/img/favicon.png')}}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
     <title>admin @yield('title')</title>
@@ -64,19 +58,19 @@
 
     <!-- CSS Files -->
 
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="{{url('../assets/css/bootstrap.min.css')}}" rel="stylesheet" />
 
 
 
 
-    <link href="../assets/css/now-ui-dashboard.minaa26.css?v=1.5.0" rel="stylesheet" />
+    <link href="{{url('../assets/css/now-ui-dashboard.minaa26.css?v=1.5.0')}}" rel="stylesheet" />
 
 
 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="../assets/demo/demo.css" rel="stylesheet" />
+    <link href="{{url('../assets/demo/demo.css')}}" rel="stylesheet" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
